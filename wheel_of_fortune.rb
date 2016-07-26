8266de615c4af6410061942e6e715175e17550e8
thayer11 <holly.coddington@gmail.com>
2016-07-25 19:24:22 -0600

    five-failures

--- wheel_of_fortune.rb
+++ wheel_of_fortune.rb
@@ -1,16 +1,28 @@
 class WheelOfFortune
-  def initialize
-    p "Happy coding!"
+attr_reader :theme, :guesses, :phrase
+  
+  def initialize(hash) 
+    @theme = hash[:theme]
+    @phrase = hash[:phrase]
+    @guesses = []
   end
-
+  
   def to_s
-    nil
+  return phrase
   end
 
   def can_i_have?(input)
-    nil
+    phrase_array = @phrase.downcase.chars
+    # p phrase_array
+    guesses.push(input)
+    if phrase_array.include? input.downcase
+      return true
+    
+    else
+      return false 
+    end
   end
-
+ 
   def game_over?
     nil
   end
